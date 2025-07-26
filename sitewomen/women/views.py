f56f2f1adca3fbb0becf91c857eb1fa3cef0b3e1
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-07-26 12:47:52 +0300

    routing

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request): # HttpRequest через цей клас буде доступна вся інформація про поточний запит
+    return HttpResponse('Page Women App')
+
+def categories(request):
+    return HttpResponse('<h1>Categories</h1>')

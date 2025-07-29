5f1c5a2ab769f7ba5cd8edfaa272b523fb82680f
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-07-29 16:25:44 +0300

    url tags ulr rlyurlyrtyrr

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,12 +8,16 @@ from django.urls import reverse
 
 # Create your views here.
 
-menu = ['Site About','Add article','Feedback','Sing up']
+menu = [{'title': "About site page", 'url_name': 'about'},
+        {'title': "Add post", 'url_name': 'add_page'},
+        {'title': "Feedback", 'url_name': 'contact'},
+        {'title': "Login", 'url_name': 'login'}
+]
 
 data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+    {'id': 1, 'title': 'Анджеліна Джолі', 'content': 'Біографія Анджеліни Джолі', 'is_published': True},
+    {'id': 2, 'title': 'Марго Роббі', 'content': 'Біографія Марго Роббі', 'is_published': False},
+    {'id': 3, 'title': 'Джулія Робертс', 'content': 'Біографія Джулії Робертс', 'is_published': True},
 ]
 
 def index(request):
@@ -22,28 +26,27 @@ def index(request):
         'title': 'Main Page',
         'menu': menu,
         'posts': data_db,
-
     }
     return render(request,'women/index.html',context=data)
+
 
 def about(request):
     return render(request,'women/about.html', {'title': 'About Page'})
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Categories</h1><p>id: {cat_id}</p>')
+def show_post(request,post_id):
+    return HttpResponse(f"Show article id: {post_id}")
 
 
-def categories_by_slug(request,cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Categories</h1><p>id(str): {cat_slug}</p>')
+def addpage(request):
+    return HttpResponse("Adding page")
 
-def archive(request, year):
-    if int(year) > 2023:
-        uri = reverse('cats', args=('music',))
-        return redirect(uri) #301 messsage
-    return HttpResponse(f'<h1>Archive by years</h1><p>{year}</p>')
 
+def contact(request):
+    return HttpResponse("Feedback")
+
+
+def login(request):
+    return HttpResponse("Authorisation")
 
 def page_not_found(request,exception):
     return HttpResponseNotFound('<h1>Page not found(((</h1>')

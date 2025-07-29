58431548463b6cd06771bbbcb3dfdf4f70bbefe1
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-07-29 11:31:26 +0300

    add templates

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import  redirect
+from django.shortcuts import redirect, render
+from django.template.context_processors import request
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 
 # Create your views here.
 
 def index(request): # HttpRequest через цей клас буде доступна вся інформація про поточний запит
-    return HttpResponse('Page Women App')
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    return render(request,'women/index.html')
+
+def about(request):
+    return render(request,'women/about.html')
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Categories</h1><p>id: {cat_id}</p>')

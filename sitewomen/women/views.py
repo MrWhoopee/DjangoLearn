e8eb5adc057027e1b989def1fb9f5184b2fc82a6
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-07-30 12:01:27 +0300

    Tags upgrade

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -21,12 +21,19 @@ data_db = [
     {'id': 3, 'title': 'Джулія Робертс', 'content': 'Біографія Джулії Робертс', 'is_published': True},
 ]
 
+cats_db = [
+    {'id': 1, 'name': 'Акторки'},
+    {'id': 2, 'name': 'Співачки'},
+    {'id': 3, 'name': 'Спортсменки'},
+]
+
 def index(request):
 
     data = {
         'title': 'Головна',
         'menu': menu,
         'posts': data_db,
+        'cat_selected': 0,
     }
     return render(request,'women/index.html',context=data)
 
@@ -48,6 +55,15 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизація")
+
+def show_category(request,cat_id):
+    data = {
+        'title': 'Відображення по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 def page_not_found(request,exception):
     return HttpResponseNotFound('<h1>Page not found(((</h1>')

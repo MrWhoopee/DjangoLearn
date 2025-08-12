b33a74346f67f67d8a519ef795512671f9d4b8c4
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-08-12 18:07:33 +0300

    custom context processors

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -1,7 +1,6 @@
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
 ]
 
 
@@ -21,12 +20,7 @@ class DataMixin:
             self.extra_context['cat_selected'] = self.cat_selected
 
 
-        if 'menu' not in self.extra_context:
-            self.extra_context['menu'] = menu
-
-
     def get_mixin_context(self,context,**kwargs):
-        context['menu'] = menu
         context['cat_selected'] = None
         context.update(kwargs)
         return context

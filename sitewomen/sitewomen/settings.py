d5d2866b862b822f3b192668d6f26b27526687c1
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-08-13 10:11:19 +0300

    Added author po posts and login_require

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, LOGIN_REDIRECT_URL
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +140,5 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
-LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'users:login'

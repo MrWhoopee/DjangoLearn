3cc299a74d0cbaca418e7876cd54ca4651833263
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-08-15 13:17:33 +0300

    Implementation of the password recovery algorithm (Custom)

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -148,3 +148,5 @@ AUTHENTICATION_BACKENDS = [
         'django.contrib.auth.backends.ModelBackend',
         'users.authentication.EmailAuthBackend',
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

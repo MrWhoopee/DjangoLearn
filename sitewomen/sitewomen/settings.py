2f6f692d2e4c3186f8147f53c14c9928036cccf5
Mr.Whoopee <artem.holovko.97@gmail.com>
2025-08-30 16:06:33 +0300

    Realized github authentication and add pipeline

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL, \
     AUTHENTICATION_BACKENDS, AUTH_USER_MODEL
 
+from sitewomen.secrets import key, secret
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -166,3 +168,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
+
+SOCIAL_AUTH_GITHUB_KEY = key
+SOCIAL_AUTH_GITHUB_SECRET = secret
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)

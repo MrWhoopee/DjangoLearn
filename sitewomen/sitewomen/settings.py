8312d40beb6d9956e86caab56f0750a9f0bd3e41
Mr_whoopee <artem.holovko.97@gmail.com>
2025-09-13 12:52:09 +0300

    connect PostgreSQL to the project

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -91,9 +91,17 @@ WSGI_APPLICATION = 'sitewomen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sitewomen_db',
+        'USER': 'sitewomen',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 

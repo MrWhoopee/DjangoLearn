f63e09d926b21aabd568484fbfb85f8e230ec71d
Mr_whoopee <artem.holovko.97@gmail.com>
2025-09-14 17:30:01 +0300

    Caching of site pages

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
+from django.views.decorators.cache import cache_page
 
 
 register_converter(converters.FourDigitYearConverter, "year4")
